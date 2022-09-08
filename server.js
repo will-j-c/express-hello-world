8b6ca533885b56aa7cf3120d60b515ff87c90f8d
Will <willjcGA@outlook.com>
2022-09-08 20:06:32 +0800

    feat: added all routers to server.js

--- server.js
+++ server.js
@@ -4,11 +4,19 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const projectRouter = require('./routes/projectRoutes');
+const authRouter = require('./routes/authRoutes');
+const commentRouter = require('./routes/commentRoutes');
+const roleRouter = require('./routes/roleRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 const port = process.env.PORT || 8800;
 
 app.use('/api/v1/projects', projectRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/roles', roleRouter);
+app.use('/api/v1/comments', commentRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(port, async () => {
   try {
