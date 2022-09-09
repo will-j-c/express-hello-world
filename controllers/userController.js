a49be599daee2d7cc122cf38d94f7f7374851149
MUI LE <tanmui1992@gmail.com>
2022-09-10 00:05:50 +0800

    userController: update showAllUser & shoUsername

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,5 +15,22 @@ const controller = {
     res.status(200);
     return res.json(users);
   },
+  showUsername: async (req, res) => {
+    const username = req.params.username;
+    try {
+      const user = await UserModel.findOne({ username }).exec();
+      console.log(user);
+      if (!user) {
+        return res.status(404).json();
+      }
+      res.status(200);
+      return res.json(user);
+    } catch (error) {
+      res.status(500);
+      return res.json({
+        error: 'Failed to fetch user by username from database',
+      });
+    }
+  },
 };
 module.exports = controller;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 
 router.get('/', userController.showAllUsers);
+router.get('/:username', userController.showUsername);
+router.get('/:username', userController.showUsername);
 
 module.exports = router;
