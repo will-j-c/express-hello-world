94f306c9bbd05b9996258fe9af3871ebed9ace6a
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-09 16:51:58 +0800

    feat: complete seeding functionality for roles

---
+++ seeds/roles/roles.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-param-reassign */
+const RoleModel = require('../../models/roleModel');
+const ProjectModel = require('../../models/projectModel');
+
+const createRoles = async (roles) => {
+  // get projectIDs from DB
+  const firstProjectID = await ProjectModel.findOne({ title: 'Happy Robot' }).exec();
+  const secondProjectID = await ProjectModel.findOne({ title: 'Happy Meal' }).exec();
+  const thirdProjectID = await ProjectModel.findOne({ title: 'An Archived Project' }).exec();
+  const fourthProjectID = await ProjectModel.findOne({ title: 'Filled Out Project' }).exec();
+
+  roles[0].project_id = firstProjectID;
+  roles[1].project_id = secondProjectID;
+  roles[2].project_id = firstProjectID;
+  roles[3].project_id = thirdProjectID;
+  roles[4].project_id = fourthProjectID;
+
+  for await (const role of roles) {
+    try {
+      await RoleModel.create(role);
+    } catch (err) {
+      console.log(`err creating role: ${err}`);
+    }
+  }
+};
+
+module.exports = createRoles;
--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,28 +1,19 @@
 /* eslint-disable no-unused-vars */
 require('./config');
 
-// const globAll = require('glob-all');
 const userSeed = require('./users/users');
 const UsersJson = require('./users/users.json');
 
 const projectsSeed = require('./projects/projects');
 const ProjectsJson = require('./projects/projects.json');
 
+const rolesSeed = require('./roles/roles');
+const RolesJson = require('./roles/roles.json');
+
 const seed = async () => {
-  // const requireAll = globAll
-  //   .sync(['seeds/users/*.js', '!seeds/config.js', '!seeds/seeds.js'])
-  //   .map((filepath) => {
-  //     console.log('Seeding: ', filepath);
-  //     // eslint-disable-next-line import/no-dynamic-require, global-require
-  //     return require(`../${filepath}`);
-  //   });
-
-  // await Promise.all(requireAll);
-  // return require(`./users/users`);
-  // process.exit(1);
-
   const usersSeeding = await userSeed(UsersJson);
   const projectsSeeding = await projectsSeed(ProjectsJson);
+  const rolesSeeding = await rolesSeed(RolesJson);
   process.exit(1);
 };
 
