ef50e83fd26f4b12c58cb915e10105f6c98d3fa2
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-11 10:35:33 +0800

    chore: set up file structures for contributor route and controller

---
+++ controllers/contributorController.js
@@ -0,0 +1,43 @@
+const ContributorModel = require('../models/contributorModel');
+const ProjectModel = require('../models/projectModel');
+const RelationshipModel = require('../models/contributorsRelationship');
+
+const controller = {
+  showAll: async (req, res) => {
+
+  },
+
+  showOne: async (req, res) => {
+
+  },
+
+  add: async (req, res) => {
+
+  },
+
+  update: async (req, res) => {
+
+  },
+
+  delete: async (req, res) => {
+
+  },
+
+  addApplicant: async (req, res) => {
+
+  },
+
+  removeApplicant: async (req, res) => {
+
+  },
+
+  acceptApplicant: async (req, res) => {
+
+  },
+
+  rejectApplicant: async (req, res) => {
+
+  },
+}
+
+module.exports = controller;
--- routes/contributorRoutes.js
+++ routes/contributorRoutes.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const controller = require('../controllers/contributorController');
+const authMiddleware = require('../middlewares/userAuth');
 
 const router = express.Router();
+
+router.put(
+  '/:id/accept/:userId',
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthorised,
+  controller.acceptApplicant
+);
+router.put(
+  '/:id/reject/:userId',
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthorised,
+  controller.rejectApplicant
+);
+router.post('/:id/apply', authMiddleware.isAuthenticated, controller.addApplicant);
+router.delete('/:id/withdraw', authMiddleware.isAuthenticated, controller.removeApplicant);
+router.get('/:id', controller.showOne);
+router.delete(
+  '/:id',
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthorised,
+  controller.delete
+);
+// eslint-disable-next-line prettier/prettier
+router.put(
+  '/:id',
+  authMiddleware.isAuthenticated,
+  authMiddleware.isAuthorised,
+  controller.update
+);
+router.get('/', controller.showAll);
+router.post('/', authMiddleware.isAuthenticated, controller.add);
 
 module.exports = router;
