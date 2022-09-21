879859b4cdb5c57c2a87e24c289babc0bcd4c7be
MUI LE <tanmui1992@gmail.com>
2022-09-22 01:16:09 +0800

    feat(seeds): add seeds data

--- seeds/contributors/contributors.js
+++ seeds/contributors/contributors.js
@@ -9,12 +9,30 @@ const createcontributors = async (contributors) => {
   const secondProjectID = await ProjectModel.findOne({ title: 'Happy Meal' }).exec();
   const thirdProjectID = await ProjectModel.findOne({ title: 'An Archived Project' }).exec();
   const fourthProjectID = await ProjectModel.findOne({ title: 'Filled Out Project' }).exec();
+  const fifthProjectID = await ProjectModel.findOne({ title: 'Appwrite' }).exec();
+  const sixthProjectID = await ProjectModel.findOne({ title: 'Templates by Equals' }).exec();
+  const seventhProjectID = await ProjectModel.findOne({ title: 'AppLogger by PLG Works' }).exec();
+  const eighthProjectID = await ProjectModel.findOne({ title: 'Spotify Audiobooks' }).exec();
+  const ninthProjectID = await ProjectModel.findOne({ title: 'Retention AI' }).exec();
+  const tenthProjectID = await ProjectModel.findOne({ title: 'Amazon Go' }).exec();
+  const eleventhProjectID = await ProjectModel.findOne({ title: 'PhotoScan by Google' }).exec();
+  const twelfthProjectID = await ProjectModel.findOne({ title: 'Google Duplex' }).exec();
+  const thirteenthProjectID = await ProjectModel.findOne({ title: 'Brain.fm' }).exec();
 
   contributors[0].project_id = firstProjectID;
   contributors[1].project_id = secondProjectID;
   contributors[2].project_id = firstProjectID;
   contributors[3].project_id = thirdProjectID;
   contributors[4].project_id = fourthProjectID;
+  contributors[5].project_id = fifthProjectID;
+  contributors[6].project_id = sixthProjectID;
+  contributors[7].project_id = seventhProjectID;
+  contributors[8].project_id = eighthProjectID;
+  contributors[9].project_id = ninthProjectID;
+  contributors[10].project_id = tenthProjectID;
+  contributors[11].project_id = eleventhProjectID;
+  contributors[12].project_id = twelfthProjectID;
+  contributors[13].project_id = thirteenthProjectID;
 
   for await (const contributor of contributors) {
     try {
--- seeds/projects/projects.js
+++ seeds/projects/projects.js
@@ -10,12 +10,22 @@ const createProjects = async (projects) => {
   const harold = await UserModel.findOne({ username: 'harold' }).exec();
   const steve = await UserModel.findOne({ username: 'steve' }).exec();
   const mcspicy = await UserModel.findOne({ username: 'mcspicy' }).exec();
+  const melody = await UserModel.findOne({ username: 'melody' }).exec();
+  const will = await UserModel.findOne({ username: 'will' }).exec();
+  const zoe = await UserModel.findOne({ username: 'zoe' }).exec();
 
   projects[0].user_id = harold._id;
   projects[1].user_id = steve._id;
   projects[2].user_id = harold._id;
   projects[3].user_id = harold._id;
-  projects[4].user_id = mcspicy._id;
+  projects[5].user_id = mcspicy._id;
+  projects[6].user_id = mcspicy._id;
+  projects[7].user_id = will._id;
+  projects[8].user_id = will._id;
+  projects[9].user_id = zoe._id;
+  projects[10].user_id = zoe._id;
+  projects[11].user_id = melody._id;
+  projects[12].user_id = melody._id;
 
   for await (const project of projects) {
     try {
